ee7552b76c006c35b616490b9e7c502bccb0b5de
Maggie Davidson <jmagdavidson@gmail.com>
2019-08-28 21:46:22 -0400

    Added comment feature

--- adlist/ads/urls.py
+++ adlist/ads/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,12 @@ urlpatterns = [
     path('<int:pk>/', views.AdsDetailView.as_view(), name='ad_detail'),
     path('create/', views.AdsCreateView.as_view(), name='ad_create'),
     path('ad/<int:pk>/update/', views.AdsUpdateView.as_view(), name='ad_update'),
-    path('ad/<int:pk>/delete/', views.AdsDeleteView.as_view(), name='ad_delete')
+    path('ad/<int:pk>/delete/', views.AdsDeleteView.as_view(), name='ad_delete'),
+    path('ad_picture/<int:pk>', views.stream_file, name='ad_picture'),
+    path('ad/<int:pk>/comment',
+        views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update',
+        views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete',
+        views.CommentDeleteView.as_view(success_url=reverse_lazy('ads')), name='comment_delete'),
 ]
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.views import View, generic
-from django.conf import settings
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from ads.models import *
 from ads.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from ads.forms import *
 
 class AdsListView(OwnerListView):
     model = Ad
@@ -13,19 +16,110 @@ class AdsDetailView(OwnerDetailView):
     model = Ad
     template_name = "ad_detail.html"
 
-class AdsCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
-    template_name = "ad_form.html"
-    success_url = reverse_lazy('ads')
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at').all()
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
-class AdsUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
-    template_name = "ad_form.html"
+# replaced this view with other view below
+# class AdsCreateView(OwnerCreateView):
+#     model = Ad
+#     fields = ['title', 'price', 'text']
+#     template_name = "ad_form.html"
+#     success_url = reverse_lazy('ads')
+
+class AdsCreateView(LoginRequiredMixin, View):
+    template = 'ads/ad_form.html'
     success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        form = CreateForm()
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Add owner to the model before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+# replaced this view with other view below
+# class AdsUpdateView(OwnerUpdateView):
+#     model = Ad
+#     fields = ['title', 'price', 'text']
+#     template_name = "ad_form.html"
+#     success_url = reverse_lazy('ads')
+
+class AdsUpdateView(LoginRequiredMixin, View):
+    template = 'ads/ad_form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk) :
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+
+        return redirect(self.success_url)
 
 class AdsDeleteView(OwnerDeleteView):
     model = Ad
     template_name = "ad_delete.html"
     success_url = reverse_lazy('ads')
+
+def stream_file(request, pk) :
+    ad = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = ad.content_type
+    response['Content-Length'] = len(ad.picture)
+    response.write(ad.picture)
+    return response
+
+# views for managing comments on ads
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        a = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=a)
+        comment.save()
+        return redirect(reverse_lazy('ad_detail', args=[pk]))
+
+class CommentUpdateView(OwnerUpdateView):
+    model = Comment
+    template_name = "ads/comment_update.html"
+    model = Ad
+    fields = ['text']
+    template_name = "comment_update.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse_lazy('ad_detail', args=[ad.id])
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse_lazy('ad_detail', args=[ad.id])
