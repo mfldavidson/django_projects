52dba34d1cbb65a0097dd4f68a85e9e63db188c0
Maggie Davidson <jmagdavidson@gmail.com>
2019-08-31 14:45:13 -0400

    added ads/ in fromt of pk for ad detail view

--- adlist/ads/urls.py
+++ adlist/ads/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AdsListView.as_view(), name='ads' ),
-    path('<int:pk>/', views.AdsDetailView.as_view(), name='ad_detail'),
+    path('ad/<int:pk>/', views.AdsDetailView.as_view(), name='ad_detail'),
     path('create/', views.AdsCreateView.as_view(), name='ad_create'),
     path('ad/<int:pk>/update/', views.AdsUpdateView.as_view(), name='ad_update'),
     path('ad/<int:pk>/delete/', views.AdsDeleteView.as_view(), name='ad_delete'),
